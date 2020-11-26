9e4db47f94bb0a8cfa06c234c501902195d9cc81
neelrahulr <rathorerahul131@gmail.com>
2020-11-26 18:42:25 +0530

    Refactored: Validated with Space

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -5,7 +5,7 @@ echo "Welcome to PIN Code Validation System"
 read -p "Enter your PIN Code : " pin
 
 #regex for PIN
-reg_pat="^[a-zA-Z]{0}[!@#$&*]{0}[0-9]{6}[a-zA-Z]{0}[!@#$&*]{0}$"
+reg_pat="^[a-zA-Z]{0}[!@#$&*]{0}[0-9]{3}[ ]?[0-9]{3}[a-zA-Z]{0}[!@#$&*]{0}$"
 
 if [[ $pin =~ $reg_pat ]]
 then
