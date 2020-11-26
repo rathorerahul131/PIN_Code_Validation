b86053fb06e14387123298554cc7392ba04d6745
neelrahulr <rathorerahul131@gmail.com>
2020-11-26 18:27:08 +0530

    Refactored: Validation for Six Digits

--- pinCodeValidation.sh
+++ pinCodeValidation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash/
 
 echo "Welcome to PIN Code Validation System"
+
+read -p "Enter your PIN Code : " pin
+#regex for PIN
+reg_pat="^[0-9]{6}$"
+
+if [[ $pin =~ $reg_pat ]]
+then
+        echo "Valid PIN Code"
+else
+        echo "Invaild PIN Code"
+fi
